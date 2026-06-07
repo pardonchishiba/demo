30007b322f444442d7c99e2fd48520b1fc7b3603
pardonchishiba <pardonchishiba@gmail.com>
2026-06-07 19:53:24 +0200

    Added 3 interface files

---
+++ src/main/resources/static/js/script.js
@@ -0,0 +1,5 @@
+// This is just a setup and not a full code
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("title").innerText = "It works!";
+    console.log("JavaScript loaded successfully");
+});
